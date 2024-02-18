197928bf368903df9c2a25c9ed850b62c89e1a55
Matthew <matthewridgway.deximo@gmail.com>
2024-02-18 21:09:32 +0800

    Implemented event listener to increase the value upon pressing the space bar

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonContainer from "./ButtonContainer";
 import Counter from "./Counter";
 import ResetButton from "./ResetButton";
@@ -8,6 +8,26 @@ export default function Card() {
     const [count, setCount] = useState(0);
 
     const isLocked = count === 5;
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.code === "Space") {
+                setCount((prev) => {
+                    const newCount = prev + 1;
+
+                    if (newCount > 5) return 5;
+
+                    return newCount;
+                });
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [count]);
 
     return (
         <div className={`card ${isLocked ? `card--limit` : ""}`}>
